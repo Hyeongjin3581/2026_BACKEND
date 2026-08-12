da758af3d10ce6a8510d652cd78bc9019e720aec
Hyeongjin3581 <poll3581@naver.com>
2026-08-12 15:30:34 +0900

    수정

--- JAVA_BACKEND/src/종합예제/controller/BoardController.java
+++ JAVA_BACKEND/src/종합예제/controller/BoardController.java
@@ -15,9 +15,12 @@ public class BoardController {
 
     public boolean save(BoardDto boardDto) {
         // TODO 1: boardDto 전달받아 DAO의 save()를 호출하고 결과 반환
+        boolean result = ib.save(boardDto);
+        return result;
     }
 
     public ArrayList<BoardDto> findAll() {
         // TODO 2: DAO의 findAll() 호출하여 결과 반환
+        
     }
 }
