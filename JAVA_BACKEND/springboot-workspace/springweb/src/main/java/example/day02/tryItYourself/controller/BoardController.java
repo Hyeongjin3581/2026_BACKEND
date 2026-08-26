4d7952510e3f597f097fa00ae7ce10740dda044a
Hyeongjin3581 <poll3581@naver.com>
2026-08-26 16:12:52 +0900

    쓸모없는거 제거

--- JAVA_BACKEND/springboot-workspace/springweb/src/main/java/example/day02/tryItYourself/controller/BoardController.java
+++ JAVA_BACKEND/springboot-workspace/springweb/src/main/java/example/day02/tryItYourself/controller/BoardController.java
@@ -12,8 +12,6 @@ import example.day02.tryItYourself.model.dto.BoardDto;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class BoardController {
