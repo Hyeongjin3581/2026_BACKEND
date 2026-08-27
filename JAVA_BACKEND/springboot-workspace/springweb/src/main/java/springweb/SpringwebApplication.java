aac60ed094fe1d854a6620a916b47d9e4aa1fd17
Hyeongjin3581 <poll3581@naver.com>
2026-08-27 10:37:59 +0900

    08/27/1h

--- JAVA_BACKEND/springboot-workspace/springweb/src/main/java/springweb/SpringwebApplication.java
+++ JAVA_BACKEND/springboot-workspace/springweb/src/main/java/springweb/SpringwebApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringwebApplication {
 
-	// [1] 해당 클래스파일 ctrl+f5 실행 후, 인터넷 켜고 localhost:8080 접속 후 Error Page
-	public static void main(String[] args) {
-		SpringApplication.run(SpringwebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringwebApplication.class, args);
+    }
 }
